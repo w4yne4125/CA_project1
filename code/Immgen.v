7425354e1c52ff5b6f4877876e7d2497ad01c7b8
b06902073 <b06902073@ntu.edu.tw>
2019-12-07 22:33:36 +0800

    need check

--- code/Immgen.v
+++ code/Immgen.v
@@ -3,7 +3,18 @@ module Immgen(
 	data_o
 )
 input [31:0] data_i;
-output reg [63:0] data_o;
+output reg [31:0] data_o;
 
-assign data_o = {{32{data_i[31]}},data_i[11:0]};
+if (data_i[6:0] == 7'b0010011 || data_i[6:0] == 7'b0000011)
+	begin
+		assign data_o = {{20{0}},data_i[31:21]};
+	end
+if (data[6:0] == 7'b0100011)
+	begin
+		assign data_o = {20{0},data_i[31:25],data_i[11:7]};
+	end
+if (data[6:0] == 7'b1100011)
+	begin
+		assign data_o = {20{0},data_i[31],data_i[7],data_i[30:25],data_i[11:8]};
+	end
 endmodule
