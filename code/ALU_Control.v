39a2e14e464c48ed5765c38d5b32ee5d7cbd4f07
b06902061 <luke19981030@gmail.com>
2019-12-08 16:34:49 +0800

    cut instr in ALU_Control

--- code/ALU_Control.v
+++ code/ALU_Control.v
@@ -6,16 +6,18 @@ module ALU_Control
 );
 
 // Ports
-input [4:0] funct_i;
+input [31:0] funct_i;
 input [1:0] ALUOp_i;
 
 output [2:0] ALUCtrl_o;
 
+assign cut = {funct_i[30], funct_i[25], funct_i[14:12]}
+
 assign ALUCtrl_o = (ALUOp_i != 2'b10) ? 3'b000 :   // Addi
-                   (funct_i[4] == 1)  ? 3'b001 :   // Sub
-                   (funct_i[3] == 1)  ? 3'b010 :   // Mul
-                   (funct_i[0] == 1)  ? 3'b011 :   // And
-                   (funct_i[1] == 1)  ? 3'b100 :   // OR
+                   (cut[4] == 1)  ? 3'b001 :   // Sub
+                   (cut[3] == 1)  ? 3'b010 :   // Mul
+                   (cut[0] == 1)  ? 3'b011 :   // And
+                   (cut[1] == 1)  ? 3'b100 :   // OR
                                         3'b101 ;   // ADD
     
 endmodule
