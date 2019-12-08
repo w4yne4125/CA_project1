ce161c639ea71220dfea40434767670e686c29fe
b06902061 <luke19981030@gmail.com>
2019-12-08 18:27:11 +0800

    ..

--- code/Immgen.v
+++ code/Immgen.v
@@ -5,16 +5,7 @@ module Immgen(
 input [31:0] data_i;
 output reg [31:0] data_o;
 
-if (data_i[6:0] == 7'b0010011 || data_i[6:0] == 7'b0000011)
-	begin
-		assign data_o = { {20{data_i[31]}} , data_i[31:20] };
-	end
-if (data[6:0] == 7'b0100011)
-	begin
-		assign data_o = { {20{data_i[31]}},data_i[31:25],data_i[11:7]};
-	end
-if (data[6:0] == 7'b1100011)
-	begin
-		assign data_o = { {20{data_i[31]}},data_i[31],data_i[7],data_i[30:25],data_i[11:8]};
-	end
+assign data_o = (data_i[6:0] == 7'b0010011 || data_i[6:0] == 7'b0000011)?  {{20{data_i[31]}} , data_i[31:20]} :
+		 (data_i[6:0] == 7'b0100011)?  {{20{data_i[31]}},data_i[31:25],data_i[11:7]} :
+										{ {20{data_i[31]}},data_i[31],data_i[7],data_i[30:25],data_i[11:8]};
 endmodule
