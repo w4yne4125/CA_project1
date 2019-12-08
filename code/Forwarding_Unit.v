0a2d8a46a3f25c2a990c3ec331f660c46678eb36
b06902061 <luke19981030@gmail.com>
2019-12-08 16:12:24 +0800

    plus assign

--- code/Forwarding_Unit.v
+++ code/Forwarding_Unit.v
@@ -20,16 +20,16 @@ input [4:0] MEM_WB_RdAddr_i;
 output [2:0] EX_RsOverride_o;
 output [2:0] EX_RdOverride_o;
 
-EX_RsOverride_o = 2'b00;
-EX_RtOverride_o = 2'b00;
+assign EX_RsOverride_o = 2'b00;
+assign EX_RtOverride_o = 2'b00;
 
 //EX hazard
-if(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i)) EX_RsOverride_o = 2'b10;
-if(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i)) EX_RtOverride_o = 2'b10;
+if(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i)) assign EX_RsOverride_o = 2'b10;
+if(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i)) assign EX_RtOverride_o = 2'b10;
 
 //MEM hazard
-if(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RsAddr_i)) EX_RsOverride_o = 2'b01;
-if(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RtAddr_i)) EX_RtOverride_o = 2'b01;
+if(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RsAddr_i)) assign EX_RsOverride_o = 2'b01;
+if(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RtAddr_i)) assign EX_RtOverride_o = 2'b01;
 
 
 
--- code/MUX32_3.v
+++ code/MUX32_3.v
@@ -6,7 +6,7 @@ input [31:0] data2_i;
 input [1:0] select_i;
 output [31:0] data_o;
 
-data_o = (select_i == 2'b00) ? data0_i :
+assign data_o = (select_i == 2'b00) ? data0_i :
 		 (select_i == 2'b10) ? data1_i :
 								data2_i;
 
