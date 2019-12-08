36f08337b9b23c18e7cafa1ad9ed2d12e34a5c85
b06902061 <luke19981030@gmail.com>
2019-12-08 16:16:26 +0800

    plus begin end

--- code/Forwarding_Unit.v
+++ code/Forwarding_Unit.v
@@ -24,13 +24,26 @@ assign EX_RsOverride_o = 2'b00;
 assign EX_RtOverride_o = 2'b00;
 
 //EX hazard
-if(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i)) assign EX_RsOverride_o = 2'b10;
-if(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i)) assign EX_RtOverride_o = 2'b10;
+if(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i))
+begin
+	assign EX_RsOverride_o = 2'b10;
+end
+
+if(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i))
+begin
+	assign EX_RtOverride_o = 2'b10;
+end
 
 //MEM hazard
-if(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RsAddr_i)) assign EX_RsOverride_o = 2'b01;
-if(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RtAddr_i)) assign EX_RtOverride_o = 2'b01;
+if(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RsAddr_i))
+begin
+	assign EX_RsOverride_o = 2'b01;
+end
 
+if(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RtAddr_i))
+begin
+	assign EX_RtOverride_o = 2'b01;
+end
 
 
 
