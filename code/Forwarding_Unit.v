9f98765f58ba11609444be9ec1fb4e7e429ba21c
w4yne4125 <wayne4125@gmail.com>
2019-12-08 17:55:14 +0800

    ><

--- code/Forwarding_Unit.v
+++ code/Forwarding_Unit.v
@@ -20,32 +20,15 @@ input [4:0] MEM_WB_RdAddr_i;
 output [2:0] EX_RsOverride_o;
 output [2:0] EX_RdOverride_o;
 
-assign EX_RsOverride_o = 2'b00;
-assign EX_RtOverride_o = 2'b00;
-
-//EX hazard
-if(EX_MEM_RegWrite_i && (EX_MEM_RdAddr_i != 0) && (EX_MEM_RdAddr_i == ID_EX_RsAddr_i))
-begin
-	assign EX_RsOverride_o = 2'b10;
-end
-
-if(EX_MEM_RegWrite_i && (EX_MEM_RdAddr_i != 0) && (EX_MEM_RdAddr_i == ID_EX_RtAddr_i))
-begin
-	assign EX_RtOverride_o = 2'b10;
-end
-
-//MEM hazard
-if(MEM_WB_RegWrite_i && !(EX_MEM_RegWrite_i && (EX_MEM_RdAddr_i != 0) && (EX_MEM_RdAddr_i == ID_EX_RsAddr_i)) && (MEM_WB_RdAddr_i == ID_EX_RsAddr_i))
-begin
-	assign EX_RsOverride_o = 2'b01;
-end
-
-if(MEM_WB_RegWrite_i && !( EX_MEM_RegWrite_i && (EX_MEM_RdAddr_i != 0) && (EX_MEM_RdAddr_i == ID_EX_RtAddr_i)) && (MEM_WB_RdAddr_i == ID_EX_RtAddr_i))
-begin
-	assign EX_RtOverride_o = 2'b01;
-end
+assign EX_RsOverride_o = (EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i)) ? 2'b10 :
+	                    (MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RsAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RsAddr_i)) ? 2'b01 :
+						2'b00;
 
 
+assign EX_RtOverride_o = (EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i)) ? 2'b10 :
+						(MEM_WB_RegWrite_i & !(EX_MEM_RegWrite_i & (EX_MEM_RdAddr_i != 0) & (EX_MEM_RdAddr_i == ID_EX_RtAddr_i) & (MEM_WB_RdAddr_i == ID_EX_RtAddr_i)) ? 2'b01 :
+						2'b00;
 
 
 endmodule
+
